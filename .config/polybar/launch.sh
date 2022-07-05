764740d9f4d20724ebaac2a10f1c6c1b0b73a98c
zuuzee123 <60211042+zuuzee123@users.noreply.github.com>
2022-07-05 03:31:26 -0400

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,6 @@ polybar-msg cmd quit
 
 # Launch bar1
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar polybar 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
